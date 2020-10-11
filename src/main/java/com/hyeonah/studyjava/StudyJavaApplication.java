38b1d005bc76fbbc0bd1437a3816aa9bdf0c0f7c
hyeoni90 <hyeona743@gmail.com>
2020-10-11 11:49:12 +0900

    feat: initial commit

---
+++ src/main/java/com/hyeonah/studyjava/StudyJavaApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonah.studyjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyeonah/studyjava/StudyJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeonah.studyjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
