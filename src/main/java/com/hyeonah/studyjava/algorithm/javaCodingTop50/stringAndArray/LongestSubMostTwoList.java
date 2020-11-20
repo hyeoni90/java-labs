501a041cfe8ec8b17f106bc067a02e3d9d9c0ead
hyeoni90 <gusk743@gmail.com>
2020-11-20 23:55:04 +0900

    feat: Add Longest Substring With At Most Two Distinct

---
+++ src/main/java/com/hyeonah/studyjava/algorithm/javaCodingTop50/stringAndArray/LongestSubMostTwoList.java
@@ -0,0 +1,43 @@
+package com.hyeonah.studyjava.algorithm.javaCodingTop50.stringAndArray;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubMostTwoList {
+
+    public static void main(String[] args) {
+        String str = "ccaabbb";
+        System.out.println(solution(str));
+    }
+
+    private static int solution(String str) {
+        int start = 0;
+        int end = 0;
+        int length = 0;
+        int counter = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        while(end < str.length()) {
+            char endChar = str.charAt(end);
+            map.put(endChar, map.getOrDefault(endChar, 0) + 1);
+
+            if (map.get(endChar) == 1) {
+                counter++;
+            }
+
+            end++;
+
+            while (counter > 2) {
+                char startChar = str.charAt(start);
+                map.put(startChar, map.get(startChar) - 1);
+                if(map.get(startChar) == 0){
+                    counter --;
+                }
+                start++;
+            }
+            length = Math.max(length, end - start);
+        }
+        return length;
+    }
+}
